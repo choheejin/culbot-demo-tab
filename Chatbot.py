e3617cf58559e9cebd241fac93f43e7663129e75
choheejin <hjcho9510@gmail.com>
2023-10-15 14:03:52 +0000

    feat: create chatbot demo

---
+++ Chatbot.py
@@ -0,0 +1,25 @@
+import openai
+import streamlit as st
+from inferece import infer
+
+with st.sidebar:
+    # openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
+    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+
+st.title("💬 Chatbot")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    response = infer(instruction="", input_text=prompt)
+    msg = {"role": "assitant", "context": response}
+    st.session_state.messages.append(msg)
+    st.chat_message("assistant").write(msg['msg'])
